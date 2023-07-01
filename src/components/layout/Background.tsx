f40cfa5966692994edf6a26519e617138e8a4444
ptentacle <48373016+ptentacle@users.noreply.github.com>
2023-07-01 12:14:15 +0100

    Update Background.tsx

--- src/components/layout/Background.tsx
+++ src/components/layout/Background.tsx
@@ -3,13 +3,19 @@ import { useConfigContext } from '../../config/provider';
 
 export function Background() {
   const { config } = useConfigContext();
-
+  var background;
+  if (typeof config?.settings.customization.backgroundImageUrl == "undefined") 
+  {
+    background = "none"
+  } else {
+    background = config?.settings.customization.backgroundImageUrl;
+  }
   return (
     <Global
       styles={{
         body: {
           minHeight: '100vh',
-          backgroundImage: `url('${config?.settings.customization.backgroundImageUrl}')` || '',
+          backgroundImage: `url('${background}')` || '',
           backgroundPosition: 'center center',
           backgroundSize: 'cover',
           backgroundRepeat: 'no-repeat',
